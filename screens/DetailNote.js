dc624c727996df94b79f53350fe8363a87b3c37a
Angelo <gabriel_propio95@hotmail.com>
2024-11-11 11:10:09 -0400

    agregado icono con rn elements, boton de prueba

--- screens/DetailNote.js
+++ screens/DetailNote.js
@@ -1,16 +1,26 @@
-import { Text, View, ScrollView } from 'react-native'
+import { Text, View, ScrollView, TouchableOpacity, Alert } from 'react-native'
 import {detailnoteStyle} from '../styles/DetailNoteStyle'
 import {useRoute} from '@react-navigation/native'
+import { Icon } from '@rneui/themed';
 
 export default function DetailNote(){
   const route = useRoute()
   const {note} = route.params;
 
+  const deleteNote = ()=>{
+    Alert.alert('IMPORTANTE', 'Estas seguro deseas eliminar esta nota?')
+  }
+
     return (
       <ScrollView contentContainerStyle={detailnoteStyle.scrollContainer}>
         <View style={detailnoteStyle.card}>
           <Text style={detailnoteStyle.title}>NOTA</Text>
-          <Text style={detailnoteStyle.titulo}>{note.titulo}</Text>
+          <View style={{justifyContent:'space-between', flexDirection:'row', alignItems:'center'}}>
+            <Text style={detailnoteStyle.titulo}>{note.titulo}</Text>
+            <TouchableOpacity style={{marginTop:25}} onPress={deleteNote}>
+              <Icon name='trash' type='ionicon' color='#FB4E4E' />
+            </TouchableOpacity>
+          </View>
           <Text style={detailnoteStyle.fecha}>{note.fecha}</Text>
           <Text style={detailnoteStyle.descripcion}>{note.descripcion}</Text>
         </View>
